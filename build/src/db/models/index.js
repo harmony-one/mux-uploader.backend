e05ea5810ce930e589b52150fdb87726878005a1
ahiipsa <ahiipsa@gmail.com>
2022-12-30 18:42:40 +0400

    Updated gitignore

--- build/src/db/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sequelize = void 0;
-const sequelize_typescript_1 = require("sequelize-typescript");
-const Video_1 = require("./Video");
-const config_1 = require("../../config");
-const dbConfig = config_1.config.db;
-exports.sequelize = new sequelize_typescript_1.Sequelize(Object.assign(Object.assign({}, dbConfig), { models: [Video_1.Video] }));
--- build/src/tests/jest.setup.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = require("../config");
-if (config_1.config.appEnv !== config_1.AppEnv.TEST) {
-    throw new Error(`Wrong env for test env: ${config_1.config.appEnv}`);
-}
