eaa747237eae6cb719792c4341de4ff935df96de
ahiipsa <ahiipsa@gmail.com>
2023-01-05 10:39:06 +0400

    Updated logger config

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,9 +1,16 @@
 import winston from "winston";
 
 export const logger = winston.createLogger({
-  format: winston.format.json(),
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
   transports: [
-    new winston.transports.File({ filename: "./logs/app.log" }),
+    new winston.transports.File({
+      filename: "./logs/app.log",
+      maxFiles: 5,
+      maxsize: 1000000,
+    }),
     new winston.transports.Console(),
   ],
 });
